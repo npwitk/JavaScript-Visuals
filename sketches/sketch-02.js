a83c204ac1f28d798bf6b8b8724c65b85c105b25
Nonprawich I. <40120130+npwitk@users.noreply.github.com>
2025-01-22 10:00:36 +0700

    Add arc

--- sketches/sketch-02.js
+++ sketches/sketch-02.js
@@ -1,12 +1,18 @@
 const canvasSketch = require("canvas-sketch");
+const math = require("canvas-sketch-util/math");
+const random = require("canvas-sketch-util/random");
 
 const settings = {
   dimensions: [1080, 1080],
 };
 
-const dagToRad = (degrees) => {
-  return (degrees / 180) * Math.PI;
-};
+// const degToRad = (degrees) => {
+//   return (degrees / 180) * Math.PI;
+// };
+
+// const randomRange = (min, max) => {
+//   return Math.random() * (max - min) + min;
+// };
 
 const sketch = () => {
   return ({ context, width, height }) => {
@@ -22,10 +28,10 @@ const sketch = () => {
     const h = height * 0.1;
     let x, y;
 
-    const num = 12;
+    const num = 40;
     const radius = width * 0.3;
     for (let i = 0; i < num; i++) {
-      const slice = dagToRad(360 / num);
+      const slice = math.degToRad(360 / num);
       const angle = slice * i;
 
       x = cx + radius * Math.sin(angle);
@@ -34,10 +40,29 @@ const sketch = () => {
       context.save();
       context.translate(x, y);
       context.rotate(-angle);
+      context.scale(random.range(0.1, 2), random.range(0.2, 0.5));
 
       context.beginPath();
-      context.rect(-w * 0.5, -h * 0.5, w, h);
+      context.rect(-w * 0.5, random.range(0, -h * 0.5), w, h);
       context.fill();
+      context.restore();
+
+      context.save();
+      context.translate(cx, cy);
+      context.rotate(-angle);
+
+      context.lineWidth = random.range(5, 20);
+
+      context.beginPath();
+      context.arc(
+        0,
+        0,
+        radius * random.range(0.7, 1.3),
+        slice * random.range(1, -8),
+        slice * random.range(1, 5)
+      );
+      context.stroke();
+
       context.restore();
     }
   };
