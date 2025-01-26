90940858f5f30eeefe6f3dc33ef2485281591d33
Nonprawich Intakaew <40120130+npwitk@users.noreply.github.com>
2025-01-26 22:42:43 +0700

    Finished up sketch-05

--- sketches/sketch-05.js
+++ sketches/sketch-05.js
@@ -1,40 +1,149 @@
 const canvasSketch = require('canvas-sketch');
+const random = require('canvas-sketch-util/random');
 
 const settings = {
-  dimensions: [ 1080, 1080 ]
+  dimensions: [ 1080, 1080 ],
 };
 
-let text = "s";
+let text = "G";
 let fontSize = 1200;
 let fontFamily = "serif";
 
-const sketch = () => {
-  return ({ context, width, height }) => {
-    context.fillStyle = 'white';
-    context.fillRect(0, 0, width, height);
+const typeCanvas = document.createElement("canvas")
+const typeContext = typeCanvas.getContext("2d")
 
-    context.fillStyle = "black";
-    context.font = fontSize + "px " + fontFamily;
-    context.textBaseline = "top";
+const sketch = ({ context, width, height }) => {
+
+  const cell =  20;
+  const cols = Math.floor(width / cell);
+  const rows = Math.floor(height / cell);
+  const numCells = cols * rows;
+
+  typeCanvas.width = cols;
+  typeCanvas.height = rows;
+  
+
+  return ({ context, width, height }) => {
+    typeContext.fillStyle = 'black';
+    typeContext.fillRect(0, 0, cols, rows);
+
+    fontSize = cols;
+
+    typeContext.fillStyle = "white";
+    typeContext.font = fontSize + "px " + fontFamily;
+    typeContext.textBaseline = "top";
     // context.textAlign = "center";
 
-    
-
-    const metrics = context.measureText(text) // Return text size!
+    const metrics = typeContext.measureText(text) // Return text size!
     const mx = metrics.actualBoundingBoxLeft * -1;
     const my = metrics.actualBoundingBoxAscent * -1;
     const mw = metrics.actualBoundingBoxLeft + metrics.actualBoundingBoxRight;
     const mh = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent; // Make it center
 
-    const x = (width - mw) * 0.5 - mx;
-    const y = (height - mh) * 0.5 - my;
+    const tx = (cols - mw) * 0.5 - mx;
+    const ty = (rows - mh) * 0.5 - my;
 
-    context.save();
-    context.translate(x, y);
+    typeContext.save();
+    typeContext.translate(tx, ty);
 
-    context.fillText(text, 0, 0)
-    context.restore();
+    typeContext.fillText(text, 0, 0)
+    typeContext.restore();
+
+    const typeData = typeContext.getImageData(0, 0, cols, rows).data;
+    // console.log(typeData)
+
+    context.drawImage(typeCanvas, 0, 0);
+
+    context.fillStyle = "black";
+    context.fillRect(0, 0, width, height)
+
+    context.textBaseline = "middle"
+    context.textAlign = "center"
+
+    for (let i = 0; i < numCells; i++) {
+      const col = i % cols;
+      const row = Math.floor(i / cols);
+
+      const x = col * cell;
+      const y = row * cell;
+
+      const r = typeData[i * 4 + 0];
+      const g = typeData[i * 4 + 1];
+      const b = typeData[i * 4 + 2];
+      const a = typeData[i * 4 + 3];
+
+      const glyph = getGlyph(r);
+
+      context.font = `${cell * 2}px ${fontFamily}`;
+      if (Math.random() < 0.1) context.font = `${cell * 6}px ${fontFamily}`;
+
+      // context.fillStyle = `rgb(${r}, ${g}, ${b})`;
+      context.fillStyle = "white";
+
+      context.save();
+      context.translate(x, y);
+      context.translate(cell * 0.5, cell * 0.5);
+      // context.fillRect(0, 0, cell, cell);
+
+      context.fillText(glyph, 0, 0)
+      context.restore();
+
+      
+    }
   };
 };
 
-canvasSketch(sketch, settings);
+const getGlyph = (v) => {
+  if (v < 50) return "";
+  if (v < 100) return ".";
+  if (v < 150) return "-";
+  if (v < 200) return "Google";
+
+  const glyphs = "_= /".split("")
+
+  return random.pick(glyphs);
+};
+
+const onKeyUp = (e) => {
+  text = e.key.toUpperCase();
+  manager.render()
+}
+
+document.addEventListener("keyup", onKeyUp);
+
+const start = async () => {
+  manager = await canvasSketch(sketch, settings);
+};
+
+start();
+
+
+
+// JS Async Function
+
+/*
+const url =  "https://picsum.photos/200";
+
+const loadMeSomeImage = (url) => {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.onload = () => resolve(img);
+    img.onerror = () => reject();
+    img.src = url;
+  });
+};
+
+const start = async () => {
+  const img = await loadMeSomeImage(url);
+  console.log("image width", img.width)
+  console.log("test");
+}
+
+// const start = () => {
+//   loadMeSomeImage(url).then(img => {
+//     console.log("image width", img.width);
+    
+//   });
+//   console.log("test");
+// }
+*/
